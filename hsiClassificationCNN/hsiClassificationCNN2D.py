8cde42c8f9674a2d78fa1b9442c62c5997fe6d48
David Ruiz Hidalgo <ruiz.david@correounivalle.edu.co>
2019-02-22 14:19:24 -0500

    loading hsi image

---
+++ hsiClassificationCNN/hsiClassificationCNN2D.py
@@ -0,0 +1,7 @@
+#RED CONVOLUCIONAL 2D - CLASIFICACION HSI 
+from paquete.cargarHsi import CargarHsi
+
+#COMIENZO PROGRAMA PRINCIPAL
+imagen = CargarHsi(2034, "Vaso adornado", 15, "Vaso de porcelana")
+
+print(imagen)
