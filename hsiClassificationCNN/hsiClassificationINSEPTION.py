d01ca4b61f53a6e04d548af727b3afc6c5a00540
David Ruiz Hidalgo <ruiz.david@correounivalle.edu.co>
2019-03-08 11:27:52 -0500

    Add prueba .py

--- hsiClassificationCNN/hsiClassificationINSEPTION.py
+++ hsiClassificationCNN/hsiClassificationINSEPTION.py
@@ -1,13 +1,15 @@
-#RED CONVOLUCIONAL 2D - CLASIFICACION HSI 
-#Se utiliza PCA para reduccion dimensional y estraccion de caracteristicas espectrales. A la red convolucional se introduce
-#una ventana sxs de la imagen original para la generacion de caracteristicas espaciales a partir de la convolucion. 
+#RED PROFUNDA CON TOPOLOGIA DE GRAFO INCEPTION - CLASIFICACION HSI 
+#Se utiliza la topologia de red de grafos INCEPTION para la inclusion de caracteristicas espectrales y espaciales en la arquitectura de 
+#la red profunda. Esto se logra utilizando redes convolucionales con diferentes tamaños de ventana; 1x1 para el manejo de caracteristicas 
+#espectrales y 3x3 o 2x2 para el manejo de posibles dependencias espaciales. Se extrae entonces un tensor 4D utilizando una ventana sxs 
+#de la imagen original.
 #Se utiliza como capa de salida un clasificador tipo Multinomial logistic regression. Todas las capas utilizan entrenamiento supervisado. 
 from paquete.cargarHsi import CargarHsi
 from paquete.prepararDatos import PrepararDatos
 from paquete.PCA import princiapalComponentAnalysis
 from keras import layers
-from keras import models
-from keras import regularizers
+from keras.models import Model
+from keras.layers import Input
 import matplotlib.pyplot as plt
 
 #CARGAR IMAGEN HSI Y GROUND TRUTH
@@ -25,6 +27,26 @@ datosEntrenamiento, etiquetasEntrenamiento, datosValidacion, etiquetasValidacion
 datosPrueba, etiquetasPrueba = preparar.extraerDatosPrueba2D(9)
 
 #DEFINICION RED CONVOLUCIONAL
+#input_tensor = Input(shape=(datosEntrenamiento.shape[1],datosEntrenamiento.shape[2],datosEntrenamiento.shape[3]))
+input_tensor = Input(shape=(28,28,3))
+# We assume the existence of a 4D input tensor 
+# Every branch has the same stride value (2), which is necessary to keep all
+# branch outputs the same size, so as to be able to concatenate them.
+branch_a = layers.Conv2D(128, 1, activation='relu', strides=2)(input_tensor)
+# In this branch, the striding occurs in the spatial convolution layer
+branch_b = layers.Conv2D(128, 1, activation='relu')(input_tensor)
+branch_b = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_b)
+# In this branch, the striding occurs in the average pooling layer
+branch_c = layers.AveragePooling2D(3, strides=2)(input_tensor)
+branch_c = layers.Conv2D(128, 3, activation='relu')(branch_c)
+branch_d = layers.Conv2D(128, 1, activation='relu')(input_tensor)
+branch_d = layers.Conv2D(128, 3, activation='relu')(branch_d)
+branch_d = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_d)
+# Finally, we concatenate the branch outputs to obtain the module output
+output = layers.concatenate([branch_a, branch_b, branch_c, branch_d], axis=-1)
+#model = Model(input_tensor,output)
+#print(model.summary())
+'''
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), kernel_regularizer=regularizers.l2(0.001),activation='relu', input_shape=(datosEntrenamiento.shape[1],datosEntrenamiento.shape[2],datosEntrenamiento.shape[3])))
 #model.add(layers.MaxPooling2D((2, 2)))
@@ -39,7 +61,8 @@ model.add(layers.Dense(64, kernel_regularizer=regularizers.l2(0.001), activation
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(17, activation='softmax'))
 print(model.summary())
-
+'''
+'''
 #ENTRENAMIENTO DE LA RED CONVOLUCIONAL
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 history = model.fit(datosEntrenamiento,etiquetasEntrenamiento,epochs=15,batch_size=512,validation_data=(datosValidacion, etiquetasValidacion))
@@ -79,3 +102,4 @@ plt.show()
 #GRAFICAS
 data.graficarHsi(groundTruth)
 data.graficarHsi(datosSalida)
+'''
---
+++ hsiClassificationCNN/pruebaPython.py
@@ -0,0 +1,7 @@
+#Archivo de Prueba en Python
+
+
+a = 1
+b = 2
+
+print(a+b)
