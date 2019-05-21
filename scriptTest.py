e40b42f573d6c2fc938b7cef5d20128d86ae8b68
David Ruiz Hidalgo <ruiz.david@correounivalle.edu.co>
2019-05-21 16:06:39 -0500

    Update

--- scriptTest.py
+++ scriptTest.py
@@ -13,10 +13,9 @@ data.graficarHsi_VS(groundTruth, imagen[15])
 
 ####### EN DESARROLLO ##########################
 # 1. Desarrollar una arquitectura de red selección de caracteristicas NO SUPERVISADA
+        # k-PCA
         # Autoendoder
-        # Boltsman machine
         # Grafo de autoencoders
         # CNN entrenada de forma no supervisada
-        # Evaluar una red tipo Inception con CNN 3D
 # 2. Desarrollar un esquema de data augmentation => prepararDatos.py
 # 4. Revisar documentacion reciente del estado del arte.
